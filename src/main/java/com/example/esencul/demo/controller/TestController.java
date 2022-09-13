0188ee358dd5fc7eea2761b79031998bc93dd59c
enesdongezJF <enes.dongez@jforce.com>
2022-09-13 14:40:20 +0300

    first commit

---
+++ src/main/java/com/example/esencul/demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.esencul.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3001")
+public class TestController {
+
+    @RequestMapping(path = "/getName" , method = RequestMethod.GET)
+    public String getName(){
+        return "klavye";
+    }
+}
